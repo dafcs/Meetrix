6a6d6b7051b358415b59d992531026e23a727919
Daniel FCS <danielf.cs@outlook.com>
2023-08-14 11:57:34 +0100

    setupid spring

---
+++ src/main/java/com/Meetrix/MeetrixApplication.java
@@ -0,0 +1,13 @@
+package com.Meetrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Meetrix/MeetrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Meetrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
